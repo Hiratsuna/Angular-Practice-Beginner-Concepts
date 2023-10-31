06c8242734e749a24c08f9bf64041bc8793882ef
Gerry Jekova <130181277+Hiratsuna@users.noreply.github.com>
2023-10-31 03:17:41 +0300

    Update star-rating.component.ts

--- src/app/feature/star-rating/star-rating.component.ts
+++ src/app/feature/star-rating/star-rating.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-star-rating',
-  templateUrl: './star-rating.component.html',
-  styleUrls: ['./star-rating.component.scss']
+  selector: 'app-star-rating',  // Selector for this component.
+  templateUrl: './star-rating.component.html',  // HTML template file for the component.
+  styleUrls: ['./star-rating.component.scss']  // Styles or CSS file for the component.
 })
 export class StarRatingComponent implements OnInit {
 
-  @Input() rating: any;
-  @Input() isReadOnly: boolean = false;
+  @Input() rating: any;  // Input property to receive the rating value.
+  @Input() isReadOnly: boolean = false;  // Input property to make the rating read-only if needed.
 
-  constructor() { /* TODO document why this constructor is empty */  }
-
-  ngOnInit(): void {
-    // TODO document why this method 'ngOnInit' is empty
+  constructor() {
+    // The constructor is intentionally empty because there's no need for additional initialization in this component.
   }
 
+  ngOnInit(): void {
+    // The ngOnInit method is intentionally empty because there's no specific initialization logic required for this component.
+  }
 }
